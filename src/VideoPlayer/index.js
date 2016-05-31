848f6e1b8d605b09fe5a4e4216c0bda3b56e8a44
Thyago Falconi <Thyago Falconi>
2016-05-31 13:32:31 +0100

    Play, pause, volume, seek features

--- src/VideoPlayer/index.js
+++ src/VideoPlayer/index.js
@@ -7,20 +7,157 @@ export default class VideoPlayer extends Component {
         super(props);
 
         this.state = {
-            player: MediaPlayer().create()
+            player: MediaPlayer().create(),
+            isPlaying: true,
+            seekPercentage: 0,
+            volumePercentage: 100,
+            isSeekBarMouseDown: false,
+            isVolumeBarMouseDown: false,
         };
 
         this.state.player.getDebug().setLogToBrowserConsole(false);
+        window.addEventListener('mouseup', this.onMouseUp);
+        window.addEventListener('mousemove', this.onMouseMove);
     }
 
     componentDidMount() {
         this.refs.video.oncontextmenu = () => { return false; };
         this.state.player.initialize(this.refs.video, 'http://dash.edgesuite.net/envivio/Envivio-dash2/manifest.mpd', true);
+              debugger;  
+        // if(localStorage.getItem('seek') > 0)
+        // {
+        //     console.log(localStorage.getItem('seek'));
+        //     this.seekTo(parseInt(localStorage.getItem('seek')));
+        // }
+        
+        // if(localStorage.getItem('volume') > 0)
+        // {
+        //     this.setVolume(parseInt(localStorage.getItem('volume')));
+        // }
+        
+        setInterval(() => {
+            if(!this.state.isSeekBarMouseDown && this.state.player.duration() > 0) {                
+                this.setState(Object.assign({
+                    seekPercentage: this.refs.video.currentTime / this.state.player.duration() * 100
+                }));
+            }
+        }, 1000);
     }
+
+    onPlayPause = () => {
+        this.setState(Object.assign({
+            isPlaying: !this.state.isPlaying
+        }));
+        
+        this.state.isPlaying ? this.refs.video.pause() : this.refs.video.play();
+    }
+    
+    onMouseUp = () => {
+        if(this.state.isSeekBarMouseDown) {
+            this.setState(Object.assign({
+                isSeekBarMouseDown: false
+            }));
+        }
+        
+        if(this.state.isVolumeBarMouseDown) {
+            this.setState(Object.assign({
+                isVolumeBarMouseDown: false
+            }));
+        }
+    }
+    
+    onNeedleMouseDown = () => {
+        this.setState(Object.assign({
+            isSeekBarMouseDown: true
+        }));
+    }
+    
+    onVolumeMouseDown = () => { 
+        this.setState(Object.assign({
+            isVolumeBarMouseDown: true
+        }));
+    }
+    
+    onMouseMove = (mouseEvent) => {    
+        if(this.state.isSeekBarMouseDown && mouseEvent.pageX >= this.refs.seekContainer.offsetLeft && mouseEvent.pageX <= this.refs.seekContainer.offsetWidth + this.refs.seekContainer.offsetLeft) {
+            this.onSeekContainerClick(mouseEvent);
+        }
+        
+        if(this.state.isVolumeBarMouseDown)
+        {
+            let videoTotalSize = this.refs.video.offsetHeight;
+            let startPositionVolumeContainer = videoTotalSize - (this.refs.volumeContainer.offsetTop * -1) -10;
+            let endPositionVolumeContainer = startPositionVolumeContainer + this.refs.volumeContainer.offsetHeight -10;
+                
+            if(this.state.isVolumeBarMouseDown && mouseEvent.pageY >= startPositionVolumeContainer - 25 && mouseEvent.pageY <= endPositionVolumeContainer - 25) {
+                this.onVolumeContainerClick(mouseEvent);
+            }
+        }
+    }
+    
+    onSeekContainerClick = (mouseEvent) => {
+        let percentage = ((mouseEvent.pageX - this.refs.seekContainer.offsetLeft) / this.refs.seekContainer.offsetWidth) * 100;
+            
+        this.setState(Object.assign({
+            seekPercentage: percentage
+        }));
+    
+        this.seekTo(percentage);
+    }
+    
+    seekTo = (percentage) => {        
+            console.log(percentage);
+        localStorage.setItem("seek", percentage);
+        
+        this.state.player.seek(percentage * this.state.player.duration() / 100);
+    }
+    
+    onVolumeContainerClick = (mouseEvent) => {
+        let videoTotalSize = this.refs.video.offsetHeight;
+        let startPositionVolumeContainer = videoTotalSize - (this.refs.volumeContainer.offsetTop * -1) -10;
+        let endPositionVolumeContainer = startPositionVolumeContainer + this.refs.volumeContainer.offsetHeight - 10;
+                
+        let percentage = (mouseEvent.pageY - startPositionVolumeContainer) / (endPositionVolumeContainer - startPositionVolumeContainer) * 100;
+        
+        let adjustedPercentage = (percentage - 100 + 30) * -1;        
+            
+        this.setState(Object.assign({
+            volumePercentage: adjustedPercentage
+        }));
+        
+        let lolzor = (percentage - 100 + 30) * -1; 
+        
+        if(lolzor <= 0)
+            this.setVolume(0);
+        else if(lolzor >= 100)
+            this.setVolume(1);
+        else
+            this.setVolume((percentage - 100 + 30) * -1);
+        
+    }  
+    
+    setVolume = (percentage) => {
+                
+        localStorage.setItem("volume", percentage);
+        
+        this.state.player.setVolume(volume / 100);
+    }
+    
 
     render() {
         const style = {};
         style.left = this.state.needleLeft;
+        style.top = this.state.needleTop;
+        
+        let playPauseClassName = (this.state.isPlaying) ? 'fa-pause' : 'fa-play';
+        
+        let seekStyle = {
+            left: (this.state.seekPercentage - 1) + '%'
+        };
+        
+        let volumeStyle = {
+            bottom: (this.state.volumePercentage - 1) + '%'
+        }
 
         return (
             <div className="sky-video-container">
@@ -37,17 +174,17 @@ export default class VideoPlayer extends Component {
                 </div>
                 <div className="controls-container">
                     <div className="controls">
-                        <i className="fa fa-play fa-lg" aria-hidden="true"></i>
-                        <div className="seek-container">
+                        <i className={`fa fa-lg ${playPauseClassName}`} aria-hidden="true" onClick={this.onPlayPause}></i>
+                        <div ref="seekContainer" className="seek-container" onClick={this.onSeekContainerClick}>
                             <div className="seek-progress">
                             </div>
-                            <div className="needle">
+                            <div className="needle" style={seekStyle} onMouseDown={this.onNeedleMouseDown}>
                             </div>
                         </div>
-                        <div className="volume-container">
+                        <div className="volume-container" >
                             <i className="fa fa-volume-up fa-lg" aria-hidden="true"></i>
-                            <div className="volume-level-container">
-                                <div className="needle">
+                            <div ref="volumeContainer" className="volume-level-container" onClick={this.onVolumeContainerClick} >
+                                <div className="needle" style={volumeStyle} onMouseDown={this.onVolumeMouseDown}>
                                 </div>
                             </div>
                         </div>
